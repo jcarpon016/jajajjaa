098d76cb0ac7e07c77ed7f10a68e7a759365f096
jcarpon016 <132546629+jcarpon016@users.noreply.github.com>
2023-05-17 20:24:18 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+let fijar_n = 0
+input.onGesture(Gesture.Shake, function () {
+    let n = 0
+    fijar_n = randint(1, 6)
+    basic.showNumber(n)
+    basic.pause(1000)
+    basic.clearScreen()
+})
